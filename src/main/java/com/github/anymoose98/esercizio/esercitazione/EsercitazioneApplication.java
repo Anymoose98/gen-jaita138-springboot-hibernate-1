666ca04f02426dc04b2bbaccae84dce8ecd47bac
Di Giacomo Daniel <daniel.digiacomo98@gmail.com>
2025-01-28 14:55:25 +0100

    first commit

---
+++ src/main/java/com/github/anymoose98/esercizio/esercitazione/EsercitazioneApplication.java
@@ -0,0 +1,13 @@
+package com.github.anymoose98.esercizio.esercitazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercitazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercitazioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/anymoose98/esercizio/esercitazione/EsercitazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.anymoose98.esercizio.esercitazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercitazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
